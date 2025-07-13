88fa5026cf327217bc16022ec45632927eba588f
James Ding <jamesding365@gmail.com>
2025-07-13 15:02:53 -0700

    Add solution for two-sum problem

---
+++ leetcode/1-two-sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        solutions = {}
+
+        for i in range(len(nums)):
+            current = nums[i]
+            complement = target - current
+
+            if current in solutions:
+                return [i, solutions[current]]
+
+            solutions[complement] = i
