dabb30d8d932c8836e3d1bd59e8494534be77f3b
James Ding <jamesding365@gmail.com>
2025-07-13 22:26:39 -0700

    Add solution for grouping anagrams

---
+++ leetcode/49-group-anagrams.py
@@ -0,0 +1,21 @@
+class Solution:
+
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        buckets = {}
+
+        for el in strs:
+            counts = [0] * 26
+
+            for c in el:
+                normalized = ord(c) - ord("a") # Normalize to 0-25 range for 'a' to 'z'
+                counts[normalized] += 1
+
+            key = tuple(counts) # Can use bytes as it is also hashable, but you are limited to 256 as a maximum count
+
+            if key not in buckets:
+                buckets[key] = []
+
+            buckets[key].append(el)
+
+        return list(buckets.values())
