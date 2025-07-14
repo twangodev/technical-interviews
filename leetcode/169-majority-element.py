0b8cbd3c340e945a87c0a9a5c78b70f8ce952195
James Ding <jamesding365@gmail.com>
2025-07-13 23:37:20 -0700

    Add solution for majority element problem

---
+++ leetcode/169-majority-element.py
@@ -0,0 +1,16 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+
+        majority = None
+        count = 0
+
+        for i in nums:
+            if count == 0:
+                majority = i
+
+            count += 1 if i == majority else -1
+
+            if count >= len(nums) / 2:
+                return i
+
+        return majority
