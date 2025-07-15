4cdfa7a4ff00401ff94777f3bb0632f4f86b646e
James Ding <jamesding365@gmail.com>
2025-07-14 20:31:00 -0700

    Add solution for sorting an array using merge sort

---
+++ leetcode/912-sort-an-array.py
@@ -0,0 +1,40 @@
+def merge(left, right):
+    result = []
+
+    i = 0
+    j = 0
+
+    while i < len(left) and j < len(right):
+        left_el = left[i]
+        right_el = right[j]
+
+        if left_el <= right_el:
+            result.append(left_el)
+            i += 1
+        else:
+            result.append(right_el)
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+
+    return result
+
+def merge_sort(nums):
+    if len(nums) <= 1:
+        return nums
+
+    center = len(nums) // 2
+
+    left = nums[:center]
+    right = nums[center:]
+
+    sorted_left = merge_sort(left)
+    sorted_right = merge_sort(right)
+
+    return merge(sorted_left, sorted_right)
+
+class Solution:
+
+    def sortArray(self, nums: List[int]) -> List[int]:
+        return merge_sort(nums)
