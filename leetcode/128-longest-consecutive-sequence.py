3ca296aa33276c823ccbc926ec8c777cebd7caf3
James Ding <jamesding365@gmail.com>
2025-07-18 22:20:18 -0700

    Add solution for longest consecutive sequence in an array

---
+++ leetcode/128-longest-consecutive-sequence.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nums = set(nums)
+        answer = 0
+
+        for n in nums:
+            previous = n - 1
+
+            if previous in nums:
+                continue
+
+            length = 1
+            while n + length in nums:
+                length += 1
+
+            answer = max(answer, length)
+
+        return answer
