5723590feefcce9d83a8debc7f0de155c9891891
James Ding <jamesding365@gmail.com>
2025-07-19 15:48:54 -0700

    Add solution for subarray sum equals k

---
+++ leetcode/560-subarray-sum-equals-k.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+
+        cumulative = {0: 1}
+
+        s = 0
+        count = 0
+
+        for n in nums:
+            s+= n
+            complement = s - k
+
+            if complement in cumulative:
+                count += cumulative[complement]
+
+            cumulative[s] = cumulative.get(s, 0) + 1
+
+        return count
