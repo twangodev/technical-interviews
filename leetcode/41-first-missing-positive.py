4a6d8e5dc12927140b4cffdcbf5822b7a2ff4439
James Ding <jamesding365@gmail.com>
2025-07-19 22:26:57 -0700

    Add solution for first missing positive integer

---
+++ leetcode/41-first-missing-positive.py
@@ -0,0 +1,22 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+
+        n = len(nums)
+
+        for i in range(n):
+            el = nums[i]
+
+            if el <= 0 or el > n:
+                nums[i] = n + 1
+
+        for i in range(n):
+            el = abs(nums[i])
+            if 1 <= el <= n:
+                target = el - 1
+                nums[target] = -abs(nums[target])
+
+        for i in range(n):
+            if nums[i] > 0:
+                return i + 1
+
+        return n + 1
